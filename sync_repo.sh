aee32959d41a2930681b593ae937d3a332964e5a
-TOSH- <tosh@t0x0sh.org>
2016-01-13 12:14:15 +0100

    Moving t0x0sh.org root directory

--- sync_repo.sh
+++ sync_repo.sh
@@ -2,16 +2,15 @@
 
 # Date : 2014-03-22
 # Author: Tosh
-# 
+#
 # Synchronise the local repo with tosh-codes
 
 set -x
 set -e
 
-REMOTE_REPO="/usr/share/nginx/www/t0x0sh"
+REMOTE_REPO="/home/t0x0sh/www/t0x0sh/"
 LOCAL_REPO="./repo"
 
 chmod -R 0755 $LOCAL_REPO
 
-rsync -avzh --delete --progress -e ssh $LOCAL_REPO www-data@t0x0sh.org:$REMOTE_REPO/
-
+rsync -avzh --delete --progress -e ssh $LOCAL_REPO t0x0sh@t0x0sh.org:$REMOTE_REPO/
--- sync_site.sh
+++ sync_site.sh
@@ -2,17 +2,16 @@
 
 # Date : 2014-03-22
 # Author: Tosh
-# 
+#
 # Synchronise the local repo with tosh-codes
 
 set -x
 set -e
 
-REMOTE_REPO="/usr/share/nginx/www/t0x0sh"
+REMOTE_REPO="/home/t0x0sh/www/t0x0sh"
 LOCAL_REPO="./output"
 
 pelican content
 chmod -R 0755 $LOCAL_REPO
 
-rsync -a --progress -e ssh $LOCAL_REPO/* www-data@t0x0sh.org:$REMOTE_REPO/
-
+rsync -a --progress -e ssh $LOCAL_REPO/* t0x0sh@t0x0sh.org:$REMOTE_REPO/
