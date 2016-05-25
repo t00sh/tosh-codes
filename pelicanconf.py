0ae4e136e584fd76836dc1786c4cc51e2e8797e8
-TOSH- <tosh@t0x0sh.org>
2016-05-25 22:13:16 +0200

    Blog: add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,14 +21,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/define__tosh__'),
+          ('Github', 'https://github.com/t00sh'),)
 
 DEFAULT_PAGINATION = 5
 
